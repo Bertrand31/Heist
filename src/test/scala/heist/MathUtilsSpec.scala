cff432a4c8ad7d9a987d5d3f45c5d96a65ab47f1
Bertrand31 <bertrandjun@gmail.com>
2021-02-13 22:15:04 +0100

    Add specs for MathUtils

---
+++ src/test/scala/heist/MathUtilsSpec.scala
@@ -0,0 +1,41 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import heist.{BankCoordinate, MathUtils}
+
+class MathUtilsSpec extends AnyFlatSpec {
+
+  behavior of "the probability calculation function"
+
+  it should "apply the relevant forumla to a 25 wide grid" in {
+
+    val probability = MathUtils.calculateProbability(25, 5)
+    assert(probability === 0.6738254512314335590792739567338941684151393037899801454013114887)
+  }
+
+  it should "apply the relevant forumla to a 100 wide grid" in {
+
+    val probability = MathUtils.calculateProbability(100, 5)
+    assert(probability === 0.9756279041567402036467525234746243790962621013547958720229022918)
+  }
+
+  it should "return 1 when distance is 0" in {
+
+    val probability = MathUtils.calculateProbability(100, 0)
+    assert(probability === 1D)
+  }
+
+  behavior of "the euclidian distance calculation function"
+
+  it should "calculate the correct distance between two distinct points" in {
+
+    val a = BankCoordinate(1.3, 3.1)
+    val b = BankCoordinate(8.0, 15.9)
+    assert(MathUtils.euclidianDistance(a, b) === 14.44749113168096)
+  }
+
+  it should "return 0 for two points with the same coordinates" in {
+
+    val a = BankCoordinate(1.3, 3.1)
+    val b = BankCoordinate(1.3, 3.1)
+    assert(MathUtils.euclidianDistance(a, b) === 0)
+  }
+}
